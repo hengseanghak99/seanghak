c3a292083f050b37329e3be336c5083bcc0bfa28
hengseanghak99 <hengseanghak10499@gmail.com>
2024-06-25 14:31:18 +0700

    add metatags

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -7,10 +6,15 @@ import HomePage from "./pages/HomePage";
 import ExEdPage from "./pages/ExEdPage";
 import NotFound from "./pages/NotFound"
 import Blogs from "./pages/Blogs";
+import { Helmet } from 'react-helmet';
 
 const App = () => {
   return (
     <Router>
+         <Helmet>
+          <title>My React App</title>
+          <meta name="description" content="Welcome to my React app" />
+        </Helmet>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/contact-me" element={<EmailMe />} />
