5bb810ee36299e30dea8a35ea2e8f2805ad02d24
hengseanghak99 <hengseanghak10499@gmail.com>
2024-06-25 11:40:27 +0700

    change react tab logo

--- src/components/SendEmail.js
+++ src/components/SendEmail.js
@@ -46,7 +46,7 @@ const ContactForm = () => {
       {/* Contact Us Card Container */}
       <div className="grid place-items-center mx-2 my-20 sm:my-auto">
         {/* Contact Us Card */}
-        <div className="w-11/12 p-12 sm:w-8/12 md:w-6/12 lg:w-5/12 2xl:w-4/12 px-6 py-10 sm:px-10 sm:py-6 border-2 border-white-500 rounded-lg shadow-md lg:shadow-lg">
+        <div className="w-11/12 p-12 sm:w-8/12 md:w-6/12 lg:w-5/12 2xl:w-4/12 px-6 py-10 sm:px-10 sm:py-6 border-2 border-white-500 rounded-lg shadow-md lg:shadow-lg bg-black">
           {/* Card Title */}
           <h2 className="text-center font-mono text-3xl lg:text-4xl text-white">
             Contact Me
