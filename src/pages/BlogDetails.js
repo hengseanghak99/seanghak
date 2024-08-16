3b659728c161d47df044991f265e4dbe021ced03
hengseanghak99 <hengseanghak10499@gmail.com>
2024-08-16 09:30:08 +0700

    add blog feature v1

--- src/pages/BlogDetails.js
+++ src/pages/BlogDetails.js
@@ -2,14 +2,14 @@ import React from "react";
 import Headers from "../components/Header";
 import Footer from "../components/Footer";
 import Particles from "../components/Particles";
-import BlogDetail from "../components/blogDetails";
+import BlogDetail from "../components/BlogDetail";
 
 const BlogDetails = () => {
   return (
     <div>
       <Particles id="tsparticles"></Particles>
       <Headers></Headers>
-     <BlogDetail></BlogDetail>
+      <BlogDetail></BlogDetail>
       <Footer></Footer>
     </div>
   );
